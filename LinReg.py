de666e5499443083a49b32a80e4e6c2902e629ad
Neil Rayu <neilrayu@gmail.com>
2021-11-28 15:07:24 -0600

    Linear Regression notebook

--- LinReg.py
+++ LinReg.py
@@ -22,10 +22,10 @@ class ScatterPlot:
         sumOfSquares = 0
         for p in points:
             sumOfSquares+= ((p-mean) ** 2)
-        return math.sqrt(sumOfSquares/2)
+        return math.sqrt(sumOfSquares/len(points))
     def correlation(self):
         zSum = 0
-        for i in range (len(self.points[0])):
+        for i in range (len(self.pointsX)):
             zSum += (((self.points[0][i]-self.meanX)/self.SDx) * ((self.points[1][i]-self.meanY)/self.SDy))
         return zSum/(len(self.points))
     def scatterInfo(self):
@@ -41,6 +41,7 @@ class ScatterPlot:
 
 
 scatter = ScatterPlot([[0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4,5]])
+print(scatter.SDx)
 print(scatter.regression)
 scatter2= ScatterPlot([[14.2,16.4,11.9,15.2,18.5,22.1,19.4,25.1,23.4,18.1,22.6,17.2], [215,325,185,332,406,522,412,614,544,421,445,408]])
 print(scatter2.info)
